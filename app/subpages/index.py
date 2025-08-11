826ee94467fc9682afff592c6011b0a130a56dbc
Gordon J. Köhn <gordon.koehn@dbsse.ethz.ch>
2025-08-11 09:41:05 +0200

    minor text

--- app/subpages/index.py
+++ app/subpages/index.py
@@ -71,7 +71,7 @@ def app():
     - *Resistance mutations*: Direct lookup of mature protein mutations known to confer resistance to antiviral drugs.
     - *Dynamic mutation heatmaps*: Amino acid mutations or Nucleotide mutations
     - *Explore variant signatures*: Plot variant-specific mutations over time.
-    - *Explore Background Mutations*: See the mutations not currently accounted for in the variant signatures we track.
+    - *Explore background mutations*: See the mutations not currently accounted for in the variant signatures we track.
     - *Rapid variant abundance*: Estimate variant abundance over time for an interactively defined set of variants.
     """)
     
