c46d4fbffc1650f94e1a1eacea6e4393bfee1017
Gordon J. Köhn <gordon.koehn@dbsse.ethz.ch>
2025-10-20 13:26:09 +0200

    bump

--- app/version.py
+++ app/version.py
@@ -1,7 +1,7 @@
 """Version information for v-pipe-scout."""
 
 # Static version information (fallback)
-VERSION = "0.4.2-beta"
+VERSION = "0.4.6-beta"
 BUILD_DATE = None
 BUILD_COMMIT = None
 BUILD_TAG = None
